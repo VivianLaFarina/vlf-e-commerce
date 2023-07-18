9498688edd0d05f84c976e54fb094811bdfd5161
Vivian La Farina <vivianlafarina@gmail.com>
2023-07-17 23:19:24 -0500

    purchases_History added :art:

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { axiosEcommerce } from '../utils/configAxios'
+import { axiosEcommerce, getConfig } from '../utils/configAxios'
+import PurchaseCard from '../components/purchases/purchaseCard'
 
 const Purchases = () => {
+    const [purchases, setPurchases] = useState([])
 
     useEffect(() => {
-        const config = {
-            headers: {
-                Authorization: "Bearer " + JSON.parse(localStorage.getItem("userInfo"))?.token
-            }
-        }
-
         axiosEcommerce
-            .get("purchases", config)
-            .then((res) => console.log(res.data))
+            .get("purchases", getConfig())
+            .then((res) => setPurchases(res.data))
             .catch((err) => console.log(err))
 
     }, [])
 
     return (
-        <main className='px-2'>
+        <main className='px-2 max-w-[1000px] mx-auto'>
             <section className='flex gap-2 items-center my-3s '>
                 <Link to='/'>Home</Link>
                 <div className=" h-[5px] aspect-square bg-red-500 rounded-full"></div>
                 <span className='font-bold'> Purchases</span>
             </section>
-            <section>
+            <section className='grid gap-6 py-6'>
+                {
+                    purchases.map(purchase => <PurchaseCard key={purchase.id} purchase={purchase} />)
+                }
+
 
 
             </section>
---
+++ src/utils/date.js
@@ -0,0 +1,10 @@
+export const formatDateDDMMYYY = (oldFormat) => {
+  const newDate = new Date(oldFormat);
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  };
+
+  return newDate.toLocaleDateString('es-Es', options);
+};
